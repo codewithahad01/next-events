3144cedf8aec9b30646409783093d7ff0faba2ae
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-20 00:17:29 +0430

    using type script and testing typescript in project

---
+++ components/Nav.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Nav() {
+    let helloWorld = "Hello World";
+    let helloWorld2: string
+    
+    interface User {
+        name: string;
+        id: number;
+      }
+
+    const user: User = {
+        name: "Hayes",
+        id: 0,
+      };
+    return (
+        <div>
+            
+            <h1>welcome to next event handler section </h1>
+        </div>
+        
+    )
+}
+
+export default Nav
