da969020e88284f35caeffc651a9b3917986956e
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-20 14:04:21 +0430

    images dir created in public dir and added three static images for events

--- dummy-data.js
+++ dummy-data.js
@@ -6,7 +6,7 @@ const DUMMY_EVENTS = [
             'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
         location: 'Somestreet 25, 12345 San Somewhereo',
         date: '2021-05-12',
-        image: 'images/coding-event.jpg',
+        image: 'images/coding-event.gif',
         isFeatured: false,
         },
         {
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,8 @@
 import React from 'react'
+import dummyData from '../../dummy-data'
 
 function fliteredEventsPage () {
+    // let filterDataEvents = dummyData.getFilteredEvents(this.state.dateFilter);
     return (
         <div>
             <h1>Filterd Events</h1>
