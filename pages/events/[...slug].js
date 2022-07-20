86a246aaa65c002411f8cabc2362ed1e6b7cd9d6
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-20 13:03:26 +0430

    events dir added for storing data for events

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function fliteredEventsPage () {
+    return (
+        <div>
+            <h1>Filterd Events</h1>
+        </div>
+    )
+}
+
+export default fliteredEventsPage
