20ffc76bf56a9be37cc46911361fb3838f67c590
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-21 12:36:47 +0430

    changed design of event item

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from "next/link"
 
 function EventItem(props) {
     const { title, image, date, location, id } = props;
@@ -15,19 +15,21 @@ function EventItem(props) {
 
 
     return (
-        <li className=''>
-        <Image src={"/" + image} alt={title} width="650" height="250" layout="fixed"></Image>
-        <div>
+        <li className=' p-6 border my-6 bg-cyan-600 text-white rounded-lg shadow-xl md:flex md:m-10'>
+        <div className="my-10">   
+            <Image src={"/" + image} alt={title} width="350" height="200" layout="fixed"></Image>
+        </div>
+        <div className="text-gray-300 md:p-4 md:my-4">
             <div>
-            <h2>{title}</h2>
+            <h2 className="text-xl font-bold md:text-3xl">{title}</h2>
             </div>
             <div>
-            <time>{humanReadableDate}</time>
+            <time className="font-bold">{humanReadableDate}</time>
             </div>
             <div>
             <address>{location}</address>
             </div>
-            <div>
+            <div className="md:mt-28 md:float-right my-2 text-purple-700 bg-yellow-400 rounded-lg p-2 shadow-xl">
             <Link href={exploreLink}>Explor Event</Link>
             </div>
         </div>
