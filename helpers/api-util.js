06db78e676dd9f73d0237c89ef7d809a9219db36
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-28 15:29:37 +0430

    api-utils section changed some funcations

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -8,14 +8,20 @@ export async function getAllEvents() {
 
     for(const key in data) {
         events.push({
+            id: key,
             ...data[key],
-            id: key
         })
     }
+    return events
 }
 
 export async function getFeaturedEvents() {
     const allEvents = await getAllEvents()
     return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+    const allEvents = await getAllEvents()
+    return allEvents.find((event) => event.id === id);
 }
 
